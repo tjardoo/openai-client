c51d451f89eb21948b400a42f2063440568f6a69
Tjardo <info@tjardo.com>
2023-04-11 16:00:56 +0200

    Re-use the http client for next requests

--- src/v1/api.rs
+++ src/v1/api.rs
@@ -16,24 +16,24 @@ use serde::de::DeserializeOwned;
 const OPENAI_API_V1_ENDPOINT: &str = "https://api.openai.com/v1";
 
 pub struct Client {
-    pub base_url: String,
-    pub api_key: String,
+    http_client: reqwest::Client,
+    base_url: String,
+    api_key: String,
 }
 
 impl Client {
     pub fn new(api_key: String) -> Self {
         Self {
+            http_client: reqwest::Client::new(),
             base_url: OPENAI_API_V1_ENDPOINT.to_string(),
             api_key,
         }
     }
 
     pub async fn get(&self, path: &str) -> Result<String, APIError> {
-        let client = reqwest::Client::new();
-
         let url = format!("{}{}", &self.base_url, path);
 
-        let response = client
+        let response = self.http_client
             .get(url)
             .header(reqwest::header::CONTENT_TYPE, "application/json")
             .bearer_auth(&self.api_key)
@@ -49,11 +49,9 @@ impl Client {
     }
 
     pub async fn post<T: Serialize>(&self, path: &str, parameters: &T) -> Result<String, APIError> {
-        let client = reqwest::Client::new();
-
         let url = format!("{}{}", &self.base_url, path);
 
-        let response = client
+        let response = self.http_client
             .post(url)
             .header(reqwest::header::CONTENT_TYPE, "application/json")
             .bearer_auth(&self.api_key)
@@ -70,11 +68,9 @@ impl Client {
     }
 
     pub async fn delete(&self, path: &str) -> Result<String, APIError> {
-        let client = reqwest::Client::new();
-
         let url = format!("{}{}", &self.base_url, path);
 
-        let response = client
+        let response = self.http_client
             .delete(url)
             .header(reqwest::header::CONTENT_TYPE, "application/json")
             .bearer_auth(&self.api_key)
@@ -90,11 +86,9 @@ impl Client {
     }
 
     pub async fn post_with_form(&self, path: &str, form: Form) -> Result<String, APIError> {
-        let client = reqwest::Client::new();
-
         let url = format!("{}{}", &self.base_url, path);
 
-        let response = client
+        let response = self.http_client
             .post(url)
             // .header(reqwest::header::CONTENT_TYPE, "multipart/form-data")
             .bearer_auth(&self.api_key)
@@ -120,11 +114,9 @@ impl Client {
         I: Serialize,
         O: DeserializeOwned + std::marker::Send + 'static,
     {
-        let client = reqwest::Client::new();
-
         let url = format!("{}{}", &self.base_url, path);
 
-        let event_source = client
+        let event_source = self.http_client
             .post(url)
             .header(reqwest::header::CONTENT_TYPE, "application/json")
             .bearer_auth(&self.api_key)
