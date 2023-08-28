86fadaf60bb3c27a99cfa62ac32ac616baa8dc72
Tjardo <info@tjardo.com>
2023-08-28 12:40:41 +0200

    Add new models + mark models as deprecated

--- src/v1/models.rs
+++ src/v1/models.rs
@@ -1,23 +1,40 @@
 use std::fmt::Display;
 use serde::{Serialize, Deserialize};
 
+// https://platform.openai.com/docs/models/overview
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum OpenAIModel {
     #[serde(rename = "gpt-4")]
     Gpt4,
-    #[serde(rename = "gpt-4-0314")]
-    Gpt4_0314,
+    #[serde(rename = "gpt-4-0613")]
+    Gpt4_0613,
     #[serde(rename = "gpt-4-32k")]
     Gpt4_32K,
-    #[serde(rename = "gpt-4-32k-0314")]
-    Gpt4_32K0314,
+    #[serde(rename = "gpt-4-32k-0613")]
+    Gpt4_32K0613,
     #[serde(rename = "gpt-3.5-turbo")]
     Gpt3_5Turbo,
+    #[serde(rename = "text-embedding-ada-002")]
+    TextEmbeddingAda002,
+    #[serde(rename = "whisper-1")]
+    Whisper1,
+    #[serde(rename = "text-moderation-stable")]
+    TextModerationStable,
+    #[serde(rename = "text-moderation-latest")]
+    TextModerationLatest,
+    #[deprecated(since = "0.2.11")]
+    #[serde(rename = "gpt-4-0314")]
+    Gpt4_0314,
+    #[deprecated(since = "0.2.11")]
+    #[serde(rename = "gpt-4-32k-0314")]
+    Gpt4_32K0314,
+    #[deprecated(since = "0.2.11")]
     #[serde(rename = "gpt-3.5-turbo-0301")]
     Gpt3_5Turbo0301,
     #[deprecated(since = "0.2.11")]
     #[serde(rename = "text-davinci-003")]
     TextDavinci003,
+    #[deprecated(since = "0.2.11")]
     #[serde(rename = "text-davinci-edit-001")]
     TextDavinciEdit001,
     #[deprecated(since = "0.2.11")]
@@ -29,14 +46,6 @@ pub enum OpenAIModel {
     #[deprecated(since = "0.2.11")]
     #[serde(rename = "text-ada-001")]
     TextAda001,
-    #[serde(rename = "text-embedding-ada-002")]
-    TextEmbeddingAda002,
-    #[serde(rename = "whisper-1")]
-    Whisper1,
-    #[serde(rename = "text-moderation-stable")]
-    TextModerationStable,
-    #[serde(rename = "text-moderation-latest")]
-    TextModerationLatest,
 }
 
 impl Display for OpenAIModel {
@@ -44,20 +53,22 @@ impl Display for OpenAIModel {
         write!(f, "{}",
             match self {
                 OpenAIModel::Gpt4 => "gpt-4",
-                OpenAIModel::Gpt4_0314 => "gpt-4-0314",
+                OpenAIModel::Gpt4_0613 => "gpt-4-0613",
                 OpenAIModel::Gpt4_32K => "gpt-4-32k",
-                OpenAIModel::Gpt4_32K0314 => "gpt-4-32k-0314",
+                OpenAIModel::Gpt4_32K0613 => "gpt-4-32k-0613",
                 OpenAIModel::Gpt3_5Turbo => "gpt-3.5-turbo-0301",
+                OpenAIModel::TextEmbeddingAda002 => "text-embedding-ada-002",
+                OpenAIModel::Whisper1 => "whisper-1",
+                OpenAIModel::TextModerationStable => "text-moderation-stable",
+                OpenAIModel::TextModerationLatest => "text-moderation-latest",
+                OpenAIModel::Gpt4_0314 => "gpt-4-0314",
+                OpenAIModel::Gpt4_32K0314 => "gpt-4-32k-0314",
                 OpenAIModel::Gpt3_5Turbo0301 => "gpt-3.5-turbo",
                 OpenAIModel::TextDavinci003 => "text-davinci-003",
                 OpenAIModel::TextDavinciEdit001 => "text-davinci-edit-001",
                 OpenAIModel::TextCurie001 => "text-curie-001",
                 OpenAIModel::TextBabbage001 => "text-babbage-001",
                 OpenAIModel::TextAda001 => "text-ada-001",
-                OpenAIModel::TextEmbeddingAda002 => "text-embedding-ada-002",
-                OpenAIModel::Whisper1 => "whisper-1",
-                OpenAIModel::TextModerationStable => "text-moderation-stable",
-                OpenAIModel::TextModerationLatest => "text-moderation-latest",
             }
         )
     }
