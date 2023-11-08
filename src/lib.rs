866539d78b5cdda1a992a65cd04e2390a384aae4
Patrick Hayes <patrick.d.hayes@gmail.com>
2023-11-08 09:31:44 -0800

    Adding documentation for function calling

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@
 //! - Chat
 //!   - [Create chat completion](#create-chat-completion)
 //!   - [Create chat completion (stream)](#create-chat-completion-stream)
+//!   - [Calling Functions](#calling-functions)
 //! - Edits (deprecated)
 //!   - [Create edit](#create-edit)
 //! - Images
@@ -233,7 +234,7 @@
 //!         frequency_penalty: None,
 //!         logit_bias: None,
 //!         user: None,
-//!         // or use ..Default::default()
+//!         ..Default::default()
 //!     };
 //!
 //!     let result = client.chat().create(parameters).await.unwrap();
@@ -286,6 +287,7 @@
 //!         frequency_penalty: None,
 //!         logit_bias: None,
 //!         user: None,
+//!         ..Default::default()
 //!     };
 //!
 //!     let mut stream = client.chat().create_stream(parameters).await.unwrap();
@@ -305,6 +307,106 @@
 //!
 //! More information: [Create chat completion](https://platform.openai.com/docs/api-reference/chat/create)
 //!
+
+
+
+//! ## Calling Functions
+//!
+//! Providing functions for the agent to call and interpret results.
+//!
+//! **URL** `https://api.openai.com/v1/chat/completions`
+//!
+//! **Method** `POST`
+//!
+//! ```rust
+//! use openai_dive::v1::api::Client;
+//! use openai_dive::v1::resources::chat_completion::{ChatCompletionParameters, ChatMessage, Role, Function, FunctionCallConfig, FunctionCall};
+//! use serde::{Deserialize, Serialize};
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     #[derive(Serialize, Deserialize)]
+//!     pub struct RandomNumber {
+//!         /// Minimum value of the random number (inclusive)
+//!         min: u32,
+//! 
+//!         /// Maximum value of the random number (inclusive)
+//!         max: u32,
+//!     }
+//! 
+//!     fn get_random_number(params: RandomNumber) -> Value {
+//!        // chosen by fair dice role, guaranteed to be random
+//!        let random = 4;
+//!        4.into()
+//!     }
+//!
+//!     let api_key = std::env::var("OPENAI_API_KEY").expect("$OPENAI_API_KEY is not set");
+//!
+//!     let client = Client::new(api_key);
+//! 
+//!     let mut messages = vec![
+//!             ChatMessage {
+//!                 role: Role::User,
+//!                 content: "Can you get a random number between 1 and 6 please?".to_string(),
+//!                 name: None,
+//!             },
+//!         ];
+//!
+//!     let parameters = ChatCompletionParameters {
+//!         model: "gpt-3.5-turbo-0613".to_string(),
+//!         messages: messages.clone(),
+//!         functions: Some([
+//!             Function {
+//!               name: "get_random_number".to_string(),
+//!               description: "Get a random number between two values".to_string(),
+//!               parameters: r#"{
+//!                    "type": "object",
+//!                    "properties": {
+//!                        "min": {
+//!                            "type": "integer",
+//!                            "description": "Minimum value of the random number (inclusive)",
+//!                        },
+//!                        "max": {
+//!                            "type": "integer",
+//!                            "description": "Maximum value of the random number (inclusive)",
+//!                        },
+//!                    }
+//!                }"#.into()
+//!              }
+//!          ]),
+//!          ..Default::default()
+//!     };
+//!
+//!     let result = client.chat().create(parameters).await.unwrap();
+//! 
+//!     if let Some(choice) = result.choices.first() {
+//!         if (choice.finish_reason == Some(FinishReason::FunctionCall)) {
+//!            if let Some(function_call) = choice.message.function_call {
+//!                if function_call.name == "get_random_number" {
+//!                    let random_number_params = serde_json::from_str(&function_call.arguments).unwrap();
+//!                    let random_number_result = get_random_number(random_number_params);
+//!                    messages.push(ChatMessage {
+//!                       role: Role::Function,
+//!                       content: Some(serde_json::to_string(&random_number_result).unwrap()),
+//!                       name: Some("get_random_number".to_string()),
+//!                    });
+//!                    
+//!                    let parameters = ChatCompletionParameters {
+//!                         model: "gpt-3.5-turbo-0613".to_string(),
+//!                         messages: messages.clone(),
+//!                         .Default::default()
+//!                     };
+//!
+//!                     let result = client.chat().create(parameters).await.unwrap();
+//!                }
+//!            }
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! More information: [Function calling](https://platform.openai.com/docs/guides/function-calling)
+//! 
 //! ## Create edit
 //!
 //! Creates a new edit for the provided input, instruction, and parameters.
