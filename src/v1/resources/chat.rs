84275166525955187e2515aad61f6ddf7fd928ee
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-11-27 19:30:34 +0100

    Fix

--- src/v1/resources/chat.rs
+++ src/v1/resources/chat.rs
@@ -313,6 +313,7 @@ impl Default for ChatCompletionParameters {
                 content: Some("Hello!".to_string()),
                 tool_calls: None,
                 name: None,
+                tool_call_id: None
             }],
             model: Gpt35Engine::Gpt35Turbo1106.to_string(),
             frequency_penalty: None,
@@ -338,6 +339,7 @@ impl Default for ChatMessage {
             content: None,
             tool_calls: None,
             name: None,
+            tool_call_id: None
         }
     }
 }
