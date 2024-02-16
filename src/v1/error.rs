7dd6918d635bb4d271db4b711a5158830307a69b
Stijn Tintel <stijn@linux-ipv6.be>
2024-02-16 12:57:31 +0200

    error: enable serialize for APIError
    This way the struct can be used to create an error response to an OpenAI
    API client.

--- src/v1/error.rs
+++ src/v1/error.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter, Result};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub enum APIError {
     EndpointError(String),
     InvalidRequestError(String),
